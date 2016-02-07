6318723b917ccda8ae750d01c39c3bb0b621acba
Colin Mitchell <muffinista@gmail.com>
2016-02-07 12:34:27 -0500

    Add pointillize, an example of manipulating the incoming image

---
+++ pointillize/sketch.js
@@ -0,0 +1,46 @@
+var img;
+var smallPoint, largePoint;
+
+function preload() {
+    var url = unescape(decodeURIComponent(window.urlParams.screenshot));
+    img = loadImage(url);
+}
+
+function setup() {
+
+    // figure out the screen dimensions
+    var display_width, display_height;
+    if ( typeof(window.urlParams) !== "undefined" ) {
+        display_width = window.urlParams.width;
+        display_height = window.urlParams.height;
+    }
+    else {
+        display_width = screen.width;
+        display_height = screen.height;
+    }
+
+    createCanvas(display_width, display_height);
+
+    smallPoint = 4;
+    largePoint = 20;
+
+    imageMode(CENTER);
+    noStroke();
+    background(0);
+
+    img.loadPixels();
+}
+
+function draw() {
+    // original script was interactive and used the mouse position.
+    //    let's try it with a random value instead
+    //  var pointillize = map(mouseX, 0, width, smallPoint, largePoint);
+    var pointillize = random(smallPoint, largePoint);
+
+    var x = floor(random(img.width));
+    var y = floor(random(img.height));
+    var pix = img.get(x, y);
+
+    fill(pix, 128);
+    ellipse(x, y, pointillize, pointillize);
+}
