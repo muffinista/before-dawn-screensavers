1b9a5dbe9e5df7d087f257d4200b421d6b860c00
Colin Mitchell <colin@muffinlabs.com>
2019-07-14 12:18:05 -0400

    Tweak code for rotation/etc

--- forest/sketch.js
+++ forest/sketch.js
@@ -1,15 +1,11 @@
-// Making it easier to use all these classes
-// Maybe this is a bad idea?
 const VerletPhysics2D = toxi.physics2d.VerletPhysics2D;
 const GravityBehavior = toxi.physics2d.behaviors.GravityBehavior;
 const VerletParticle2D = toxi.physics2d.VerletParticle2D;
 const VerletSpring2D = toxi.physics2d.VerletSpring2D;
-// VerletConstrainedSpring2D
-const VerletMinDistanceSpring2D = toxi.physics2d.VerletMinDistanceSpring2D;
 const Vec2D = toxi.geom.Vec2D;
 const Rect = toxi.geom.Rect;
 
-const SPRING_STRENGTH = 0.0;
+const SPRING_STRENGTH = 0.01;
 const WIGGLE = 0.3;
 const MIN_LENGTH = 30;
 const MAX_LENGTH = 50;
@@ -18,7 +14,6 @@ const MAX_LENGTH = 50;
 // Reference to physics world
 let physics;
 let trunk;
-let woods = [];
 let BRANCH_ANGLE;
 
 let w = 800;
@@ -33,18 +28,15 @@ function setup() {
 
   // Initialize the physics
   physics = new VerletPhysics2D();
-//  physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.1)));
-  // f = new toxi.physics2d.behaviors.ConstantForceBehavior(new Vec2D(0, 0));
-  // physics.addBehavior(f);
-//  physics.addBehavior(new toxi.physics2d.behaviors.ConstantForceBehavior(new Vec2D(0.01, 0)));
+  //  physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.1)));
+  //  physics.addBehavior(new toxi.physics2d.behaviors.ConstantForceBehavior(new Vec2D(0.01, 0)));
 
   // Set the world's bounding box
   physics.setWorldBounds(new Rect(0, 0, width, height));
-//  physics.setDrag(1.0);
+  physics.setDrag(0.5);
 
   let base = new Particle(w/2, h);
   trunk = new Wood(base, 50, 7, -HALF_PI);
-  woods.push(trunk);
   trunk.p1.lock();
   trunk.p2.lock();
 
@@ -55,11 +47,7 @@ function draw() {
   physics.update();
 
   background(135, 206, 235);
-
-//  trunk.display();
-  for ( let w in woods ) {
-    woods[w].display();
-  }
+  trunk.display();
 }
 
 // Child class 
@@ -86,13 +74,14 @@ class Leaf extends VerletParticle2D {
 class Wood {
   constructor(p1, length, size, angle) {
     this.p1 = p1;
-    this.p2 = new Particle(this.p1.x + length * cos(angle), this.p1.y + length * sin(angle));
-
-    let d = dist(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
 
     this.length = length;
     this.size = size;
     this.angle = angle;
+
+    this.calculateEndPoint();
+
+    let d = dist(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
     this.spring = new VerletSpring2D(this.p1, this.p2, d, SPRING_STRENGTH);
 
     physics.addParticle(this.p1);
@@ -107,25 +96,39 @@ class Wood {
     }
   }
 
+  calculateEndPoint() {
+    let x = this.p1.x + this.length * cos(this.angle);
+    let y = this.p1.y + this.length * sin(this.angle);
+
+    if ( ! this.p2 ) {
+      this.p2 = new Particle(x, y, 10);
+    }
+    else {
+      this.p2.x = x;
+      this.p2.y = y;
+    }
+  }
+
   addBranches() {
     let chance = random();
+    this.branches = [];
 
     if ( chance > 0.2 ) {
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle - BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle + BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
     }
     else if ( chance > 0.1 ) {
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle - BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle + BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle - BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
     }
     else {
-      woods.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
+      this.branches.push(new Wood(this.p2, random(MIN_LENGTH, MAX_LENGTH), this.size - 1,
         this.angle + BRANCH_ANGLE + random(-WIGGLE, WIGGLE)));
     }
   }
@@ -138,12 +141,30 @@ class Wood {
     physics.addSpring(this.leafSpring);
   }
 
+  rotate(t) {
+    this.angle += t;
+    this.calculateEndPoint();
+
+    for ( let w in this.branches ) {
+      this.branches[w].rotate(t);
+    }
+
+    if ( this.leaf ) {
+      this.leaf.x = this.p2.x;
+      this.leaf.y = this.p2.y;
+    }
+  }
+
   // Draw the branch
   display() {
     stroke(83, 53, 10);
     strokeWeight(this.size * 2);
     noFill();
     line(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
+
+    for ( let w in this.branches ) {
+      this.branches[w].display();
+    }
 
     if ( this.leaf ) {
       this.leaf.display();
