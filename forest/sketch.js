455e98f488b2ebf3e902a3d141191feac7194441
Colin Mitchell <colin@muffinlabs.com>
2019-07-15 14:17:27 -0400

    Remove physics

--- forest/sketch.js
+++ forest/sketch.js
@@ -1,66 +1,36 @@
-const VerletPhysics2D = toxi.physics2d.VerletPhysics2D;
-const GravityBehavior = toxi.physics2d.behaviors.GravityBehavior;
-const VerletParticle2D = toxi.physics2d.VerletParticle2D;
-const VerletSpring2D = toxi.physics2d.VerletSpring2D;
-const Vec2D = toxi.geom.Vec2D;
-const Rect = toxi.geom.Rect;
-
-const SPRING_STRENGTH = 0.01;
 const WIGGLE = 0.3;
 const MIN_LENGTH = 30;
 const MAX_LENGTH = 50;
+const START_THICKNESS = 7;
 
-
-// Reference to physics world
-let physics;
 let trunk;
 let BRANCH_ANGLE;
 
 let w = 800;
 let h = 600;
 
-var f;
-
 function setup() {
   BRANCH_ANGLE = HALF_PI/5;
 
   createCanvas(w, h);
 
-  // Initialize the physics
-  physics = new VerletPhysics2D();
-  //  physics.addBehavior(new GravityBehavior(new Vec2D(0, 0.1)));
-  //  physics.addBehavior(new toxi.physics2d.behaviors.ConstantForceBehavior(new Vec2D(0.01, 0)));
-
-  // Set the world's bounding box
-  physics.setWorldBounds(new Rect(0, 0, width, height));
-  physics.setDrag(0.5);
-
   let base = new Particle(w/2, h);
-  trunk = new Wood(base, 50, 7, -HALF_PI);
-  trunk.p1.lock();
-  trunk.p2.lock();
-
+  trunk = new Wood(base, MAX_LENGTH, START_THICKNESS, -HALF_PI);
   frameRate(10);
 }
 
 function draw() {
-  physics.update();
-
   background(135, 206, 235);
   trunk.display();
 }
 
 // Child class 
-class Particle extends VerletParticle2D {
-  constructor(x, y) {
-    super(new Vec2D(x, y), 0.01);
-    // this.radius = 4;
-  }
+class Particle extends p5.Vector {
 }
 
-class Leaf extends VerletParticle2D {
+class Leaf extends Particle {
   constructor(x, y) {
-    super(new Vec2D(x, y), 0);
+    super(x, y);
     this.radius = 12;
   }
 
@@ -82,11 +52,6 @@ class Wood {
     this.calculateEndPoint();
 
     let d = dist(this.p1.x, this.p1.y, this.p2.x, this.p2.y);
-    this.spring = new VerletSpring2D(this.p1, this.p2, d, SPRING_STRENGTH);
-
-    physics.addParticle(this.p1);
-    physics.addParticle(this.p2);
-    physics.addSpring(this.spring);
 
     if ( this.size > 1 ) {
       this.addBranches();
@@ -135,10 +100,6 @@ class Wood {
 
   addLeaf() {
     this.leaf = new Leaf(this.p2.x, this.p2.y)
-    this.leafSpring = new VerletSpring2D(this.p2, this.leaf, 0, SPRING_STRENGTH);
-
-    physics.addParticle(this.leaf);
-    physics.addSpring(this.leafSpring);
   }
 
   rotate(t) {
