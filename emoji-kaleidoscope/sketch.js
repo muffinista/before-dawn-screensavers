ec15160f557b6affcb6073b6e3faea407df5122a
Colin Mitchell <muffinista@gmail.com>
2021-06-11 12:56:18 -0400

    Cleanup output code

--- emoji-kaleidoscope/sketch.js
+++ emoji-kaleidoscope/sketch.js
@@ -12,13 +12,17 @@ var num_sprites = 10;
 let spriteImage;
 
 let vector;
-let slices = 12;
+let slices = 6;
 let slicer;
 
 let xMult = 10;
-let yMult = 10;
+let yMult = 0;
 
 const sheetSize = 10;
+
+const outputScale = 1.0;
+let rotation = 0;
+
 
 function preload() {
   sprites = [];
@@ -88,18 +92,20 @@ function draw() {
 
   const img = slicer.toImage(vector, imgWidth, imgHeight);
 
-  // emit(img, width / 8, height / 8, slices);
-  // emit(img, width * 0.8, height / 8, slices);
+  translate(width / 2, height / 2);
+  rotate(rotation);
+  rotation += 0.005;
 
-  // emit(img, width / 8, height * 0.8, slices);
-  // emit(img, width * 0.8, height * 0.8, slices);
-
-  emit(img, width / 2, height / 2, slices);
+  emit(img, -width/2, -height/2, slices);
+  emit(img, width/2, height/2, slices);
+  emit(img, width/2, -height/2, slices);
+  emit(img, -width/2, height/2, slices);
+  emit(img, 0, 0, slices);
 
   img.remove();
 
   vector.x += xMult;
-//  vector.y += yMult;
+  vector.y += yMult;
 
   if ( vector.x > slicer.w ) {
     vector.x = 0;
@@ -107,14 +113,7 @@ function draw() {
   if ( vector.y > slicer.h ) {
     vector.y = 0;
   }
-  // if ( vector.x > spriteImage.width * 2 || vector.x <= 0 ) {
-  //   xMult *= -1;
-  // }
-  // if ( vector.y > spriteImage.height * 2 || vector.y <= 0 ) {
-  //   yMult *= -1;
-  // }
 }
-
 
 class SliceBuilder {
   constructor(source, slices) {
@@ -127,54 +126,63 @@ class SliceBuilder {
 
     // make an arc/slice of the source image just a little bit wider than what we need
     this.e = radians(360/slices + .1);
-    // this.mask.arc(0, 0, 2 * this.w, 2 * this.h, 0, this.e);
-    this.mask.arc(0, 0, 3 * this.w, 3 * this.h, 0, this.e);
+    this.mask.arc(0, 0, 2 * this.w, 2 * this.h, 0, this.e);
+    // this.mask.arc(0, 0, 3 * this.w, 3 * this.h, 0, this.e);
+
+    this.sheet = createGraphics(this.w, this.h);
+    this.sheet.clear();
+    for( let i = 0; i < 200; i++ ) {
+      let idx = int(random(0, num_sprites));
+      this.sheet.push();
+      this.sheet.rotate(random([0, PI / 2, PI, PI * 1.5]));
+      this.sheet.image(sprites[idx], random(0, source.width * sheetSize), random(0, source.height * sheetSize));
+      this.sheet.pop();
+    }
 
     //
     // make a copy of the image with copies of the image on all sides, for wrapping
     //
-    this.base = createGraphics(this.w, this.h);
-    for ( let x = 0; x < sheetSize; x++ ) {
-      for ( let y = 0; y < sheetSize; y++ ) {
-        let idx = int(random(0, num_sprites));
-        this.base.image(sprites[idx], x * source.width, y * source.height);
-        //this.base.image(sprites[idx], random(0, source.width * sheetSize), random(0, source.height * sheetSize));
-      }
-    }
+    this.base = createGraphics(this.w * 2, this.h * 2);
+    this.base.clear();
+    this.base.image(this.sheet, 0, 0, this.w, this.h, 0, 0);
+    this.base.image(this.sheet, this.w, 0, this.w, this.h, 0, 0);
+    this.base.image(this.sheet, this.w, this.h, this.w, this.h, 0, 0);
+    this.base.image(this.sheet, 0, this.h, this.w, this.h, 0, 0);
+
+
+    //     for ( let x = 0; x < sheetSize; x++ ) {
+//       for ( let y = 0; y < sheetSize; y++ ) {
+//         let idx = int(random(0, num_sprites));
+// //        this.base.image(sprites[idx], x * source.width, y * source.height);
+//         this.base.push();
+//         this.base.rotate(random([0, PI / 2, PI, PI * 1.5]));
+//         this.base.image(sprites[idx], random(0, source.width * sheetSize), random(0, source.height * sheetSize));
+//         this.base.pop();
+//       }
+//     }
     this.slice = createImage(this.w, this.h); 
   }
 
   toSlice(vector) {
     this.slice = this.base.get(vector.x, vector.y, this.w, this.h);
-
-    // copy overlaps
-    this.slice.copy(this.base, 0, vector.y, this.w, this.h, this.w - vector.x, vector.y, this.w, this.h);
-    this.slice.copy(this.base, vector.x, 0, this.w, this.h, 0, vector.y, this.w, this.h);
-    this.slice.copy(this.base, this.w - vector.x, this.h - vector.y, this.w, this.h, vector.x, vector.y, this.w, this.h);
-
     this.slice.mask(this.mask); 
     return this.slice;
   }
 
   toImage(vector, width, height) {
-    let slice = this.toSlice(vector, width, height);
-    let img = createGraphics(width * 2, height * 2);
+    let slice = this.toSlice(vector);
+    let img = createGraphics(width * outputScale, height * outputScale);
+    // let img = createGraphics(width * 2, height * 2);
   
     for(let k = 0; k <= slices ;k++){ 
-      img.rotate( k * radians(360/(slices/2)) ); 
-      img.image(slice, 0, 0, width * 2, height * 2); 
+      // rotate and output the slice
+      img.rotate(k * radians(360/(slices/2)) ); 
+      img.image(slice, 0, 0, width * outputScale, height * outputScale); 
+
+      // mirror the slice
       img.scale(-1.0, 1.0);
-      img.image(slice, 0, 0, width * 2, height * 2);
+      img.image(slice, 0, 0, width * outputScale, height * outputScale);
     } 
-  
-    // crop square
-    let output = createGraphics(width, height);
-    output.image(img, 
-      0, 0, width, height,
-      width * 0.25, height * 0.25, width, height
-    );
-  
-    img.remove();
-    return output;
+    return img;
   }
 }
