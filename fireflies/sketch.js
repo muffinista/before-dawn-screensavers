4cdeab81a3fcd029e2df1019aa7d55a784179d42
Colin Mitchell <muffinista@gmail.com>
2021-06-20 13:04:50 -0400

    Add some states

--- fireflies/sketch.js
+++ fireflies/sketch.js
@@ -8,14 +8,13 @@ let alpha_bump = window.urlParams['Fade Rate'] || 75; // how much to fade alpha 
 
 let pg;
 
+const states = ['dark', 'dark', 'blinking', 'blinking', 'blinking', 'streaming'];
+
 function setup() {
   if ( typeof(window.urlParams) !== "undefined" ) {
     display_width = window.urlParams.width;
     display_height = window.urlParams.height;
-
-    if ( typeof(window.urlParams.density) !== "undefined" ) {
-      count = window.urlParams.density * 2;
-    }
+    density = window.urlParams.density;
   }
   else {
     display_width = screen.width;
@@ -48,44 +47,18 @@ function setup() {
 
   for(let i = 0; i < density; i++ ) {
     const offset = noise(i) * 10000;
-    const f = new Firefly(random(0, width), random(0, height),
-      (firefly, ticks) => {
-
-        if ( firefly.lastChange + 15000 <= ticks ||
-          (firefly.lastChange + 5000 <= ticks && random() > firefly.switchChance) ) {
-          firefly.switchChance = random();
-          firefly.lastChange = ticks;
-          firefly.state = firefly.state == 'dark' ? 'active' : 'dark';
-        }
-
-        if ( firefly.state == 'dark' ) {
-          return 0;
-        }
-        else {
-          const slowAmt = (offset + ticks) / 2000;
-          const slowVal = (sin(slowAmt) + 1.0) * 128;
-          if ( slowVal < minBrightness ) {
-            return 0;
-          }
-  
-          const amt = (offset + ticks) / 10;
-          const val = (sin(amt) + 1.0) * 128;
-          if ( val < minBrightness ) {
-            return 0;
-          }
-          return val;  
-        }
-      });
+    const f = new Firefly(random(0, width), random(0, height));
     fireflies.push(f);
   }
 
   // we're going to draw to a PGraphics object, here it is
   pg = createGraphics(width, height);
-  pg.background(0);
+  pg.noSmooth();
+  pg.background(0, 0, 20);
 }
 
 function draw() {
-  background(0);
+  background(0, 0, 20);
 
   for (let firefly of fireflies) {
     firefly.update();
@@ -112,28 +85,35 @@ function draw() {
   //
   // let's iterate through each pixel and drop the alpha a bit
   //
-  for (var i = 3; i < pg.pixels.length ; i += 4) {
-    pg.pixels[i] = pg.pixels[i] > 0 ? pg.pixels[i] - alpha_bump : 0;
+  // for (var i = 3; i < pg.pixels.length ; i += 4) {
+  for (var i = 3, len = pg.pixels.length; i < len ; i += 4) {
+    let val = pg.pixels[i];
+    pg.pixels[i] = val > 0 ? val - alpha_bump : 0;
   }
+
   pg.updatePixels();
   pg.imageData = pg.pixels = null;
 }
 
-
 class Firefly {
-  constructor(x, y, pulseFn) {
+  constructor(x, y) {
     this.lastChange = 0;
     this.switchChance = random();
-
+    this.offset = random() * 10000;
     this.acceleration = createVector(0, 0);
     this.velocity = createVector(random(-1, 1), random(-1, 1));
     this.position = createVector(x, y);
-    this.r = 2 + random() * 3.0;
+    this.r = 4 + random() * 3.0;
     this.maxspeed = 3; // Maximum speed
     this.maxforce = 0.05; // Maximum steering force
-    this.pulseFn = pulseFn;
     this.minBrightness = minBrightness;
+    this.switchState();
+  }
 
+  switchState() {
+    this.lastChange = millis();
+    this.switchChance = random();
+    this.state = random(states);
   }
 
   update() {
@@ -163,9 +143,44 @@ class Firefly {
     this.brightness = this.pulseFn(this, frameCount);
   }
 
+  pulseFn(frameCount) {
+    const ticks = millis();
+
+    if ( this.lastChange + 15000 <= ticks ||
+      (this.lastChange + 5000 <= ticks && random() > this.switchChance) ) {
+      this.switchState();
+    }
+
+    let cyclePerSecond = TWO_PI / 1000 / 2;
+
+    if ( this.state == 'dark' ) {
+      return 0;
+    }
+    else if ( this.state === 'blinking' ) {
+      const val = abs(sin(((this.offset + ticks) * cyclePerSecond * 0.25))) * 255;
+      if ( val > 250 ) {
+        return val;
+      }
+    }
+    else if ( this.state === 'streaming' ) {
+      const slowAmt = (this.offset + ticks) / 10000;
+      const slowVal = (sin(slowAmt) + 1.0) * 128;
+      if ( slowVal < 100 ) {
+        return 0;
+      }
+
+      const amt = (this.offset + ticks) / 5;
+      const val = (sin(amt) + 1.0) * 128;
+      if ( val < 240 ) {
+        return 0;
+      }
+      return val;  
+    }
+  }
+
   draw(pg) {
     if ( this.brightness >= this.minBrightness ) {
-      pg.fill(0, this.brightness, 0);
+      pg.fill(40, this.brightness, 0);
       pg.ellipse(this.position.x, this.position.y, this.r, this.r);  
     }
   }
