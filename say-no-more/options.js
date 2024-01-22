9f440d37075832da5de1b64d6a6e68743a315375
maxlefou <buttbadger@gmail.com>
2024-01-22 15:35:26 +0100

    updates on cavestory and saynomore

--- say-no-more/options.js
+++ say-no-more/options.js
@@ -1,8 +1,12 @@
-var
-  // Put here the number of miliseconds you want for the quotes to change. 
-  // Emulates the "Show Every" option of the original.
-  // By default, it's 10 seconds, so it's 10000.
-  secs = 10000,
+let
+  // Put here the number of seconds you want for the quotes to change. 
+  // Emulates the "Show Every" option of the original. Min was 10, max was 100.
+  // Default is 10 seconds.
+  secs = 10,
+
+  // Put here the name of the txt file that contains the quotes without the txt extension.
+  // Default is "saywhat", from saywhat.txt, the collection of the original screensaver's quotes.
+  quotes = "saywhat",
 
   // Put here the style of desired borders for the quotes box.
   // Somewhat emulates the "Draw Border" option of the original.
@@ -30,3 +34,22 @@ var
   // Default is "1.5em"
   fontSize = "1.5em";
 
+////////////////////////////////////////////////////////////////////////////
+// STUFF FOR BEFORE DAWN (ignore this part)
+
+// Fetches the parameters in the query url.
+var tmpParams = new URLSearchParams(document.location.search);
+window.urlParams = {};
+
+for(let k of tmpParams.keys() ) {
+  window.urlParams[k] = tmpParams.get(k);
+}
+
+// If the parameters 'exists' (which is normally the case in beforedawn),
+// it replaces the variables previously set by the ones set in beforedawn.
+if (window.urlParams['quotes'] != null ) quotes = window.urlParams['quotes'];
+if (window.urlParams['secs'] != null ) secs = window.urlParams['secs'];
+if (window.urlParams['borderStyle'] != null ) borderStyle = window.urlParams['borderStyle'];
+if (window.urlParams['borderColor'] != null ) borderColor = window.urlParams['borderColor'];
+if (window.urlParams['fontStyle'] != null ) fontStyle = window.urlParams['fontStyle'];
+if (window.urlParams['fontSize'] != null ) fontSize = window.urlParams['fontSize']+"em";
